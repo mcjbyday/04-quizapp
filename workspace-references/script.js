b72e553e9f4c3d01100ae0da0ab5411bc1984ea8
MCJBYDAY <jacobsmichaelc@gmail.com>
2022-04-01 14:47:20 -0500

    split into neverending version and game over prototypes

--- workspace-references/script.js
+++ workspace-references/script.js
@@ -1,7 +1,8 @@
 // DONE make a timer that starts at 60 and goes down to 0
-// TODO A 'next button' that will switch to the next object in an array of objects and display each object on the page.
+// DONE A 'next button' that will switch to the next object in an array of objects and display each object on the page.
 // DONE Then you could change the display on the page when the timer hits 0
-
+// use local storage to contain your scores
+// potential secondary 'end game' page that leverages grab local storage to display results 
 
 var firstTag = document.querySelector("#firsttag");
 var secondTag = document.querySelector("#secondtag");
@@ -10,32 +11,40 @@ secondTag.innerText = " ";
 var timerInSeconds = 60;
 var disp1 = document.querySelector("#objectpropdisplay1");
 var disp2 = document.querySelector("#objectpropdisplay2");
-
+var specialButton = document.querySelector("#special-button");
 
 let someBooksFoods = [
-    {favBook: "Bible",favFood: "Eggs"},
+    {favBook: "Bible", favFood: "Eggs"},
     {favBook: "Communist Manifesto", favFood: "Corn"},
     {favBook: "Twilight", favFood: "Potatos"},
-    {favBook: "Animal Farm", favFood: "Prunes"} ]
+    {favBook: "Animal Farm", favFood: "Prunes"} ];
 
 let currentBookFood = 0;
 
 
-specialButton.addEventListener("click", nextBookFood)
-
 setTime();
 
+specialButton.addEventListener("click", nextBookFood);
+
+function gameOver() {
+    disp1.innerText = "THAT'S A WRAP JACK";
+    disp2.setAttribute("style","display:none;");
+}
 
 function showBookFood() {
-    disp1 = someBooksFoods[currentBookFood].favBook;
-    disp2 = someBooksFoods[currentBookFood].favFood;
+    disp1.innerText = someBooksFoods[currentBookFood].favBook;
+    disp2.innerText = someBooksFoods[currentBookFood].favFood;
 }
 
 function nextBookFood() {
     currentBookFood++;
     showBookFood();
-    if (currentBookFood === someBooksFoods.length) {
-        currentBookFood = 0;
+    if (currentBookFood === (someBooksFoods.length - 1)) {
+        // neverending version
+        // currentBookFood = 0;
+        //  version with finality
+        gameOver();
+        
     }
 }
 
