2b67304feb2dfac2a20a6e7dacf74a233c759d37
MCJBYDAY <jacobsmichaelc@gmail.com>
2022-04-04 17:38:40 -0500

    improved text and element positioning in the browser using CSS

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,7 @@
 // DONE create timer
 // DONE create question objects (prompt, startwer 1, startwer 2, startwer 3, startwer 4)
 // create score
-// create content
+// DONE create content
 // local storage
 // enter player name
 
@@ -15,8 +15,11 @@ let main = document.querySelector(".main_container");
 let promptContainer = document.querySelector(".prompt_container");
 let answerContainer = document.querySelector(".answer_container");
 let menuButtonStyle = "font-size: 20pt; color: white; background-color: rgba(82, 27, 107, 0.8); border-radius:20px; border: 1px solid white; padding: 8%;"
-let answerButtonStyle = "font-size: 16pt; color: white; background-color: var(--dark3); border-radius:10px; border: 1px solid white; padding: 5%;"
+let answerButtonStyle = "font-size: 16pt; color: white; background-color: var(--dark3); border-radius:10px; border: 1px solid white; margin: 5px; padding: 8%;"
+let viewSpacingStartPromptContainer = "font-size: 32pt;font-weight:900;display: flex;flex-direction: column;align-items: center;justify-content: space-around;min-height: 50vh;"
+let viewSpacingGameplayPromptContainer = "font-size: 32pt;font-weight:900;display: flex;flex-direction: column;align-items: center;justify-content: space-around;min-height: 15vh;"
 
+let questionCounter = 0;
 var timerInSeconds = 60;
 
 // setup prototype datastructure quiz content
@@ -38,6 +41,8 @@ let playerData = [
 // let quizContent = [
 //     {questionOrder: 0, prompt: "This is the prompt of question 1", altOption1: "Alpha", altOption2: "Bravo", altOption3: "Charlie", correctOption: "Correct"}]
 
+// Initialize promptcontainer spacing 
+promptContainer.setAttribute("style",viewSpacingStartPromptContainer);
 
 // Initialize start button in DOM and set properties
 let startButton = document.createElement("button");
@@ -47,16 +52,13 @@ startButton.textContent = "Start Game";
 startButton.id = ("start_game");
 promptContainer.appendChild(startButton);
 
-// TO DO make this disappear on click, and instead display question 1?
-// on startbutton click, the timer starts currently
-// DONE make this click call the first question
-
-
-// start timer only
-// startButton.addEventListener("click", setTime);
 // start timer and display question only
 startButton.addEventListener("click", function (event) {
+  // adjust promptcontainer spacing for gameplay 
+  promptContainer.setAttribute("style",viewSpacingGameplayPromptContainer),
+  // kickoff timer 
   setTime(),
+  // display question 1 
   displayQuestion();  
 });
 
@@ -80,32 +82,37 @@ function setTime() {
         //location.href = "./scoreboard.html"
       }
   
-    }, 100);
+    }, 1000);
   }
 
 
 
 // on click, the timer starts currently
 function displayQuestion() {
+  
   //hide the start button on question and timer start;
   document.querySelector('#start_game').setAttribute("style", "display: none;");
   // set the prompt container to the question stem
-  promptContainer.innerText = quizContent[0].prompt;
+  promptContainer.innerText = quizContent[questionCounter].prompt;
   
   // make a randomized order array for the available answer options
   let ansDispSeq = randomOrderPick();
   // get the answer array from the content file for HTML display
-  let as = [quizContent[0].options[ansDispSeq[0]], quizContent[0].options[ansDispSeq[1]], quizContent[0].options[ansDispSeq[2]], quizContent[0].options[ansDispSeq[3]]];
+  let as = [quizContent[questionCounter].options[ansDispSeq[0]], quizContent[questionCounter].options[ansDispSeq[1]], quizContent[questionCounter].options[ansDispSeq[2]], quizContent[questionCounter].options[ansDispSeq[3]]];
   // loop through question's answer options and display them as selectable buttons
   for (i=0; i < as.length; i++) {
     let answerButton = document.createElement("button");
     answerButton.setAttribute("style", answerButtonStyle);
-    answerButton.className = ("ans_button");
+    answerButton.className = ("answer_item");
     answerButton.textContent = as[i];
     answerContainer.appendChild(answerButton);
   }
+
 }
 
+function inputFeedback() {
+
+}
 
 function randomOrderPick() {
   let randOrder = [];
