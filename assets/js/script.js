29adc46796e222cc9a1ca2b48551454fd33b1cd0
MCJBYDAY <jacobsmichaelc@gmail.com>
2022-04-04 16:42:52 -0500

    injecting answer option buttons into HTML

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,8 +13,9 @@ let highscoreContainer = document.querySelector(".highscore_container");
 let timerContainer = document.querySelector(".timer_container");
 let main = document.querySelector(".main_container");
 let promptContainer = document.querySelector(".prompt_container");
-let startwersContainer = document.querySelector(".startwer_container");
+let answerContainer = document.querySelector(".answer_container");
 let menuButtonStyle = "font-size: 20pt; color: white; background-color: rgba(82, 27, 107, 0.8); border-radius:20px; border: 1px solid white; padding: 8%;"
+let answerButtonStyle = "font-size: 16pt; color: white; background-color: var(--dark3); border-radius:10px; border: 1px solid white; padding: 5%;"
 
 var timerInSeconds = 60;
 
@@ -33,17 +34,17 @@ let quizContent = [
 let playerData = [
   {playerName: "", playerScore: 0}];
 
-// initialize and set start button parameters
+// // initialize and set start button parameters
 // let quizContent = [
-//     {questionOrder: 0, prompt: "This is the prompt of question 1", altOption1: "Alpha", altOption2: "Bravo", altOption3: "Charlie", correctOption: "Correct"},
+//     {questionOrder: 0, prompt: "This is the prompt of question 1", altOption1: "Alpha", altOption2: "Bravo", altOption3: "Charlie", correctOption: "Correct"}]
 
 
-// // Initialize start button in DOM and set properties
-// let startButton = document.createElement("button");
-// startButton.setAttribute("style",menuButtonStyle);
-// startButton.className = ("menu_button");
-// startButton.textContent = "Start Game";
-// promptContainer.appendChild(startButton);
+// Initialize start button in DOM and set properties
+let startButton = document.createElement("button");
+startButton.setAttribute("style",menuButtonStyle);
+startButton.className = ("menu_button");
+startButton.textContent = "Start Game";
+promptContainer.appendChild(startButton);
 
 // TO DO make this disappear on click, and instead display question 1?
 // on startbutton click, the timer starts currently
@@ -74,47 +75,46 @@ function randomOrderPick() {
 
 // on click, the timer starts currently
 function displayQuestion() {
-  // TO DO will iterate through this list
-  // let 
-  // let q = quizContent[0].prompt;
+  
+  // make a randomized order array for the available answer options
   let ansDispSeq = randomOrderPick();
+  // get the answer array from the content file for HTML display
   let as = [quizContent[0].options[ansDispSeq[0]], quizContent[0].options[ansDispSeq[1]], quizContent[0].options[ansDispSeq[2]], quizContent[0].options[ansDispSeq[3]]];
-  // let startwerButton = document.createElement("button");
-  // console.log(as);
-  // startwerButton.setAttribute("style",menuButtonStyle);
-  // startwerButton.className = ("menu_button");
-  // startwerButton.textContent = "Start Game";
-  // promptContainer.appendChild(startButton);
+  // loop through question's answer options and display them as selectable buttons
+  for (i=0; i < as.length; i++) {
+    let answerButton = document.createElement("button");
+    answerButton.setAttribute("style", answerButtonStyle);
+    answerButton.className = ("ans_button");
+    answerButton.textContent = as[i];
+    answerContainer.appendChild(answerButton);
+  }
 }
 
 
-// startButton.addEventListener("click", function () {
-//   setTime,
-//   displayQuestion;  
-// });
+startButton.addEventListener("click", function (event) {
+  setTime(),
+  displayQuestion();  
+});
 
-// function displayQuestion() {
 
-// }
-
-// function setTime() {
-//     // Sets interval in variable
-//     var timerInterval = setInterval(function() {
-//         timerInSeconds--;
-//         timerContainer.innerText = timerInSeconds;
+function setTime() {
+    // Sets interval in variable
+    var timerInterval = setInterval(function() {
+        timerInSeconds--;
+        timerContainer.innerText = timerInSeconds;
   
-//       if(timerInSeconds <= 0) {
-//         // Stops execution of action at set interval
-//         clearInterval(timerInterval);
-//         // Calls function to change display on page
-//         // resets the counter
-//         // secondTag.innerText = "You've run out of time buddy";
-//         timerContainer.innerText = "Time's up!";
+      if(timerInSeconds <= 0) {
+        // Stops execution of action at set interval
+        clearInterval(timerInterval);
+        // Calls function to change display on page
+        // resets the counter
+        // secondTag.innerText = "You've run out of time buddy";
+        timerContainer.innerText = "Time's up!";
         
-//         //pseudo code for redirecting someone to a scoreboard page
+        //pseudo code for redirecting someone to a scoreboard page
 
-//         //location.href = "./scoreboard.html"
-//       }
+        //location.href = "./scoreboard.html"
+      }
   
-//     }, 100);
-//   }
+    }, 100);
+  }
