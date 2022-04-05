96294513dd5c328a4557bc6ac8c69e03849076bc
MCJBYDAY <jacobsmichaelc@gmail.com>
2022-04-05 15:01:50 -0500

    added intro content

--- assets/js/script.js
+++ assets/js/script.js
@@ -24,10 +24,10 @@ let timerContainer = document.querySelector(".timer_container");
 let main = document.querySelector(".main_container");
 let promptContainer = document.querySelector(".prompt_container");
 let answerContainer = document.querySelector(".answer_container");
-let menuButtonStyle = "font-size: 18pt; color: white; background-color: var(--dark3); border-radius:20px; border: 1px solid white; padding: 8%;"
-let answerButtonStyle = "font-size: 14pt; color: white; background-color: var(--dark3); border-radius:10px; border: 1px solid white; margin: 5px; padding: 2%;"
-let viewSpacingStartPromptContainer = "font-size: 32pt;font-weight:900;display: flex;flex-direction: column;align-items: center;justify-content: space-around;min-height: 50vh;"
-let viewSpacingGameplayPromptContainer = "font-size: 32pt;font-weight:900;display: flex;flex-direction: column;align-items: center;justify-content: space-around;min-height: 15vh;"
+let menuButtonStyle = "font-size: 18pt; color: white; background-color: var(--dark3); border-radius:20px; border: 1px solid white; padding: 8%;";
+let answerButtonStyle = "font-size: 14pt; color: white; background-color: var(--dark3); border-radius:10px; border: 1px solid white; margin: 5px; padding: 2%;";
+let viewSpacingStartPromptContainer = "font-size: 32pt;font-weight:900;display: flex;flex-direction: column;align-items: center;justify-content: space-around;min-height: 50vh;";
+let viewSpacingGameplayPromptContainer = "font-size: 32pt;font-weight:900;display: flex;flex-direction: column;align-items: center;justify-content: space-around;min-height: 15vh;";
 
 // setup prototype datastructure quiz content
 let quizContent = [
@@ -50,21 +50,31 @@ let playerData = [
 promptContainer.setAttribute("style",viewSpacingStartPromptContainer);
 
 // Initialize start button in DOM and set properties
+let introContent = document.createElement("p");
+introContent.setAttribute("style", "font-size: 16pt; margin: 5px; padding: 2%;");
+introContent.id = ("intro_content");
+introContent.innerText = "Try to complete this quiz in 60 seconds. Incorrect answers will subtract 10 seconds. Good luck!";
+promptContainer.appendChild(introContent);
+
+
+// Initialize start button in DOM and set properties
 let startButton = document.createElement("button");
 startButton.setAttribute("style",menuButtonStyle);
 startButton.className = ("menu_button");
 startButton.textContent = "Start Game";
 startButton.id = ("start_game");
-promptContainer.appendChild(startButton);
+answerContainer.appendChild(startButton);
 
-// start timer and display question only
+
 startButton.addEventListener("click", function (event) {
   // adjust promptcontainer spacing for gameplay 
   document.querySelector('#start_game').setAttribute("style", "display: none;"),
+  document.querySelector('#intro_content').remove();
   promptContainer.setAttribute("style",viewSpacingGameplayPromptContainer)
   playGame();
 });
 
+// start timer and display question one
 function playGame() {
   // kickoff timer 
   setTime();
