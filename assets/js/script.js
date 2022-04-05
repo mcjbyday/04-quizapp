1431196671ef69d880ac64abcbdcd61c806a32f0
MCJBYDAY <jacobsmichaelc@gmail.com>
2022-04-05 15:10:04 -0500

    fixed start button sizing

--- assets/js/script.js
+++ assets/js/script.js
@@ -24,9 +24,9 @@ let timerContainer = document.querySelector(".timer_container");
 let main = document.querySelector(".main_container");
 let promptContainer = document.querySelector(".prompt_container");
 let answerContainer = document.querySelector(".answer_container");
-let menuButtonStyle = "font-size: 18pt; color: white; background-color: var(--dark3); border-radius:20px; border: 1px solid white; padding: 1%;";
+let menuButtonStyle = "font-size: 18pt; color: white; background-color: var(--dark3); border-radius:20px; border: 1px solid white; padding: 1%; min-height:100px;min-width:150px;";
 let answerButtonStyle = "font-size: 14pt; color: white; background-color: var(--dark3); border-radius:10px; border: 1px solid white; margin: 5px; padding: 2%;";
-let viewSpacingStartPromptContainer = "font-size: 32pt;font-weight:900;display: flex;flex-direction: column;align-items: center;min-height: 30vh;";
+let viewSpacingStartPromptContainer = "font-size: 32pt;font-weight:900;display: flex;flex-direction: column;align-items: center;min-height: 15vh;";
 let viewSpacingGameplayPromptContainer = "font-size: 32pt;font-weight:900;display: flex;flex-direction: column;align-items: center;justify-content: space-around;min-height: 15vh;";
 
 // setup prototype datastructure quiz content
