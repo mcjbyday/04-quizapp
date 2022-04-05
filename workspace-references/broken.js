9052683a5f2d56bea10e27f81a4509c740f97162
MCJBYDAY <jacobsmichaelc@gmail.com>
2022-04-05 09:54:36 -0500

    cycling through question types and updating content display

--- workspace-references/broken.js
+++ workspace-references/broken.js
@@ -1,14 +1,11 @@
 // TO DO
-// track scoring
-// update timer for incorrect answers
-// local storage - update only at end of quiz
-// scoreboard prototype - render DOM using local storage
-// scoreboard styling
+// create correct selections 
+// create scoring
+// create content
+// local storage
 // enter player name
-// write content
 
 // DONE
-// create correct selections 
 // create timer
 // create question objects (prompt, startwer 1, startwer 2, startwer 3, startwer 4)
 // content prototype
@@ -26,22 +23,33 @@ let answerButtonStyle = "font-size: 16pt; color: white; background-color: var(--
 let viewSpacingStartPromptContainer = "font-size: 32pt;font-weight:900;display: flex;flex-direction: column;align-items: center;justify-content: space-around;min-height: 50vh;"
 let viewSpacingGameplayPromptContainer = "font-size: 32pt;font-weight:900;display: flex;flex-direction: column;align-items: center;justify-content: space-around;min-height: 15vh;"
 
+
+
 // setup prototype datastructure quiz content
 let quizContent = [
     {prompt: "This is the prompt of question 1", options: ["Alpha", "Bravo", "Charlie", "Correct"], correctOption: "Correct"},
-    {prompt: "This is the prompt of question 2", options: ["Delta", "Echo", "Foxtrot", "Correct"], correctOption: "Correct"},
-    {prompt: "This is the prompt of question 3", options: ["Golf", "Hotel", "India", "Correct"], correctOption: "Correct"},
-    {prompt: "This is the prompt of question 4", options: ["Juliet", "Kilo", "Lima", "Correct"], correctOption: "Correct"}
-  ];
+    {prompt: "This is the prompt of question 2", options: ["Delta", "Echo", "Foxtrot", "Correct"], correctOption: "Correct"}];
 
 let questionCounter = 0;
 let questionTotal = quizContent.length;
 console.log(questionTotal);
 var timerInSeconds = 60;
+let isDisplaying = false;
+
+// setup datastructure quiz content and prompts FULL
+// let quizContent = [
+//     {questionOrder: 0, prompt: "This is the prompt of question 1", altOption1: "Alpha", altOption2: "Bravo", altOption3: "Charlie", correctOption: "Correct"},
+//     {questionOrder: 1, prompt: "This is the prompt of question 2", altOption1: "Alpha", altOption2: "Bravo", altOption3: "Charlie", correctOption: "Correct"},
+//     {questionOrder: 2, prompt: "This is the prompt of question 3", altOption1: "Alpha", altOption2: "Bravo", altOption3: "Charlie", correctOption: "Correct"},
+//     {questionOrder: 3, prompt: "This is the prompt of question 4", altOption1: "Alpha", altOption2: "Bravo", altOption3: "Charlie", correctOption: "Correct"}];
 
 // setup local player storage data to save score from page to page
 let playerData = [
   {playerName: "", playerScore: 0}];
+
+// // initialize and set start button parameters
+// let quizContent = [
+//     {questionOrder: 0, prompt: "This is the prompt of question 1", altOption1: "Alpha", altOption2: "Bravo", altOption3: "Charlie", correctOption: "Correct"}]
 
 // Initialize promptcontainer spacing 
 promptContainer.setAttribute("style",viewSpacingStartPromptContainer);
@@ -58,16 +66,22 @@ promptContainer.appendChild(startButton);
 startButton.addEventListener("click", function (event) {
   // adjust promptcontainer spacing for gameplay 
   document.querySelector('#start_game').setAttribute("style", "display: none;"),
-  promptContainer.setAttribute("style",viewSpacingGameplayPromptContainer)
+  promptContainer.setAttribute("style",viewSpacingGameplayPromptContainer),
+  isDisplaying = true,
   playGame();
 });
 
 function playGame() {
   // kickoff timer 
   setTime();
-  
+  // isDisplaying = true;
   displayQuestion();
+  inputFeedback(); 
+  // hideQuestion();
+  
+  // console.log("game over fam")
 }
+
 
 function setTime() {
     // Sets interval in variable
@@ -80,18 +94,24 @@ function setTime() {
         clearInterval(timerInterval);
         // Calls function to change display on page
         // resets the counter
-        timerContainer.innerText = "Time's up!"; 
+        // secondTag.innerText = "You've run out of time buddy";
+        timerContainer.innerText = "Time's up!";
+        
         //pseudo code for redirecting someone to a scoreboard page
         //location.href = "./scoreboard.html"
       }
+  
     }, 1000);
   }
 
+
 // on click, the timer starts currently
 function displayQuestion() {
+  if (isDisplaying === false) {
+    return;
+  }
   // set the prompt container to the question stem
   console.log("question number is " + questionCounter);
-  promptContainer.innerText = "";
   promptContainer.innerText = quizContent[questionCounter].prompt;
   // make a randomized order array for the available answer options
   let ansDispSeq = randomOrderPick();
@@ -105,54 +125,52 @@ function displayQuestion() {
     answerButton.textContent = as[i];
     answerContainer.appendChild(answerButton);
   }
-  inputFeedback();
+  isDisplaying = true;
 }
 
 
 function inputFeedback() {
-  let ans = document.querySelectorAll('.answer_item');
-  console.log("inputFeedback");
-  // add event listeners to all the answer options, isDisplaying correct if they're correct, else not correct
-  for (i = 0; i < ans.length; i++) {
-    ans[i].addEventListener("click", function (event) {
-      console.log(event.target);
-      console.log(event.target.innerText);
-      if (event.target.innerText === quizContent[questionCounter].correctOption) {
-        console.log("that's correct mang");
-        questionCounter++;
-        hideQuestionSetNext();
-      }
-      else if (event.target.innerText != quizContent[questionCounter].correctOption) {
-        console.log("not correct mang :(");
-        
-        timerInSeconds = timerInSeconds - 10;
-        questionCounter++;
-        hideQuestionSetNext();
-
-      }
-    });
+  if (isDisplaying === true) {
+    let ans = document.querySelectorAll('.answer_item');
+    // console.log(ans);
+    // add event listeners to all the answer options, isDisplaying correct if they're correct, else not correct
+    for (i=0;i < ans.length; i++) {
+      ans[i].addEventListener("click", function (event) {
+        console.log(event.target);
+        console.log(event.target.innerText);
+        if (event.target.innerText === quizContent[questionCounter].correctOption) {
+          console.log("that's correct mang");
+          questionCounter++;
+          hideQuestion();
+        }
+        else if (event.target.innerText != quizContent[questionCounter].correctOption) {
+          console.log("not correct mang :(");
+          questionCounter++;
+          hideQuestion();
+        }
+      });
+    }
   }
 }
 
-function hideQuestionSetNext() {
-  let ans = document.querySelectorAll('.answer_item');
-  ans.forEach(function(items) {
-    items.remove();
-  });
-  if (questionCounter === questionTotal) {
-    //pseudo code for redirecting someone to a scoreboard page
-    //location.href = "./scoreboard.html"
-    console.log("game over fam");
+function hideQuestion() {
+  if (isDisplaying === true) {
+    promptContainer.innerText = "Next question...";
+    let ans = document.querySelectorAll('.answer_item');
+    ans.forEach(function(items) {
+      items.setAttribute("style", "display: none;");
+    });
+    isDisplaying = false;
+    return;
   }
-  else {
-    displayQuestion();
-  }
+  isDisplaying = true;
 }
   
 
 function randomOrderPick() {
   let randOrder = [];
   let startOrder = [0,1,2,3];
+
   for (i = 0; i < 4; i++) {
     randomNumber = Math.floor(Math.random()*startOrder.length);
     // console.log("iteration" + i + "randomnumber =\n" + randomNumber);
